15d97cb4b8510cf98faa66bae2d25a5a6bb90613
Toasty-Waffles <123521136+Toasty-Waffles@users.noreply.github.com>
2023-03-19 19:07:21 -0400

    just saving work

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+const startButton = document.getElementById('start-btn');
+const quizBoxElement = document.getElementsByClassName('quiz-box')
+
+startButton.addEventListener('click', startQuiz);
+
+function startQuiz() {
+	console.log('Started');
+	startButton.classList.add('hide');
+	quizBoxElement = document.getElementsByClassName.remove('hide')
+}
+
+function startGame {
+
+}
+
+function setNextQuestion {
+
+}
+
+function selectAnswer {
+
+}
+
