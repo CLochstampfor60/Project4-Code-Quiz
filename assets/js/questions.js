02e72f19941a92a1959c0d5d3db0b9426090720c
Toasty-Waffles <123521136+Toasty-Waffles@users.noreply.github.com>
2023-03-19 22:20:04 -0400

    finished end table and started questions

---
+++ assets/js/questions.js
@@ -0,0 +1,111 @@
+
+// Array of Questions
+const quiz = [
+{
+	q: 'The full form of CSS is:',
+	options: ['Cascading Style Sheets', 'Coloured Special Sheets', 'Color and Style Sheets', 'None of the above'],
+	answer: 0
+},
+{
+	q: 'How can we change the background color of an element?',
+	options: ['background-color', 'color', 'Both A and B', 'None of the above'],
+	answer: 0
+},
+{
+	q: 'How can we change the text color of an element?',
+	options: ['background-color', 'color', 'Both A and B', 'None of the above'],
+	answer: 1
+},
+{
+	q: 'In how many ways can CSS be written in?',
+	options: ['1', '2', '3', '4'],
+	answer: 3
+},
+{
+	q: 'What type of CSS is the following code snippet? <h1 style="color:blue;">A Blue Heading</h1>',
+	options: ['Inline', 'Internal', 'External', 'None of the above'],
+	answer: 0
+},
+{
+	q: 'What type of CSS is generally recommended for designing large web pages?',
+	options: ['Inline', 'Internal', 'External ', 'None of the above'],
+	answer: 2
+},
+{
+	q: 'Which HTML tag is used to declare internal CSS?',
+	options: ['<style> ', '<link>', '<script>', 'None of the above'],
+	answer: 0
+},
+{
+	q: 'How can we select an element with a specific ID in CSS?',
+	options: ['#', '.', '^', 'None of the above'],
+	answer: 0
+},
+{
+	q: 'How can we select an element with a specific Class in CSS?',
+	options: ['#', '.', '^', 'None of the above'],
+	answer: 1
+},
+{
+	q: 'How can we write comments in CSS?',
+	options: ['/* */', '//', '#', 'All of the above'],
+	answer: 0
+},
+{
+	q: 'In the below code snippet, in what order will the margins be added? p {margin: 25px 50px 75px 100px;}',
+	options: ['Top, Right, Bottom, Left', 'Top, Left, Bottom, Right', 'Top, Bottom, Right, Left', 'Right, Left, Top, Bottom'],
+	answer: 0
+},
+{
+	q: 'Can negative values be allowed in padding property?',
+	options: ['Yes', 'No', 'Depends on the property', 'None of the above'],
+	answer: 1
+},
+{
+	q: 'The CSS property used to specify the transparency of an element is?',
+	options: ['opacity ', 'visibility', 'filter', 'None of the above'],
+	answer: 0
+},
+{
+	q: 'How can we specify the spacing between each letter in a text in CSS?',
+	options: ['alpha-spacing', 'letter-spacing', 'character-spacing', 'None of the above'],
+	answer: 1
+},
+{
+	q: 'What is the correct syntax for referring an external CSS?',
+	options: ['<link rel="stylesheet" type="text/css" href="style.css">', '<style rel="stylesheet" type="text/css" href="style.css">', 'Both A and B', 'None of the above'],
+	answer: 0
+},
+{
+	q: '',
+	options: ['', '', '', 'None of the above'],
+	answer:
+},
+{
+	q: 'HTML stands for -',
+	options: ['', '', '', 'None of the above'],
+	answer:
+},
+{
+	q: '',
+	options: ['', '', '', 'None of the above'],
+	answer:
+},
+{
+	q: '',
+	options: ['', '', '', 'None of the above'],
+	answer:
+},
+{
+	q: '',
+	options: ['', '', '', 'None of the above'],
+	answer:
+},
+{
+	q: '',
+	options: ['', '', '', 'None of the above'],
+	answer:
+},
+]
+
+
